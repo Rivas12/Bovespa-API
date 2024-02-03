8347ea13d47707eee6a9b3b048c53cd44fc57995
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-02 22:36:39 -0300

    novo tratamento de do tipo IndexOutOfBoundsException para setores que estiverem fora do rage de 0-43

--- src/main/java/com/bovespaApi/infra/RestExceptionHandler.java
+++ src/main/java/com/bovespaApi/infra/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.net.UnknownHostException;
 
@@ -19,6 +20,15 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         RestErrorMessage threatresponse = new RestErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Site não encontrado, verifique se escreveu o endereço corretamente");
         // Retorna uma mensagem de erro amigável para o usuário
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(threatresponse);
+    }
+
+    // Método para tratar exceção de recurso não encontrado
+    @ExceptionHandler(IndexOutOfBoundsException.class)
+    private ResponseEntity<RestErrorMessage> listHandler(Exception ex) {
+        // Cria um objeto RestErrorMessage com a mensagem de erro
+        RestErrorMessage threatresponse = new RestErrorMessage(HttpStatus.BAD_GATEWAY, ex.getMessage());
+        // Retorna uma mensagem de erro amigável para o usuário
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(threatresponse);
     }
 
 }
