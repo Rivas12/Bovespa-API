1ff95ed8591eda295231cb9010e2e5dc1199408d
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-07 19:17:46 -0300

    feat: endpoint e service movido para os novos arquivos

--- src/main/java/com/bovespaApi/controllers/AcaoController.java
+++ src/main/java/com/bovespaApi/controllers/AcaoController.java
@@ -7,11 +7,16 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import com.bovespaApi.services.SetorService;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -25,6 +30,12 @@ public class AcaoController {
     // Construtor com a injeção de dependência do serviço de ações
     public AcaoController(AcaoService acaoService) {
         this.acaoService = acaoService;
+    }
+
+    @GetMapping("/acao/{papel}")
+    @Operation(summary = "JSON com todos os papeis e indicadores", description = "")
+    public List<Map<String, String>> getPapel(@PathVariable String papel) throws IOException {
+        return acaoService.getPapel(papel);
     }
 
 }
--- src/main/java/com/bovespaApi/services/AcaoService.java
+++ src/main/java/com/bovespaApi/services/AcaoService.java
@@ -13,5 +13,88 @@ import java.util.*;
 @Service
 public class AcaoService {
 
+    // SOLID - Single Responsibility Principle
+    // Aqui realiza é setagem de linha(tr) da tabela no Json
+    // A regra de negócio é separada do método principal
+    private void setarDadoDaTabelaNoJson(Elements linha, List<Map<String, String>> data) {
+
+        // Cria um mapa para representar a linha
+        Map<String, String> rowMap = new LinkedHashMap<>();
+
+        // Adiciona os dados ao mapa
+        rowMap.put("Papel", linha.get(0).text());
+        rowMap.put("Empresa", linha.get(0).select("span").attr("title"));
+        rowMap.put("Cotação", linha.get(1).text());
+        rowMap.put("P/L", linha.get(2).text());
+        rowMap.put("PV/P", linha.get(3).text());
+        rowMap.put("PSR", linha.get(4).text());
+        rowMap.put("Div.Yield", linha.get(5).text());
+        rowMap.put("P/Ativo", linha.get(6).text());
+        rowMap.put("P/Cap.Giro", linha.get(7).text());
+        rowMap.put("P/EBIT", linha.get(8).text());
+        rowMap.put("P/Ativo Circ.Liq", linha.get(9).text());
+        rowMap.put("EV/EBIT", linha.get(10).text());
+        rowMap.put("EV/EBITDA", linha.get(11).text());
+        rowMap.put("Mrg Ebit", linha.get(12).text());
+        rowMap.put("Mrg. Líq.", linha.get(13).text());
+        rowMap.put("Liq. Corr.", linha.get(14).text());
+        rowMap.put("ROIC", linha.get(15).text());
+        rowMap.put("ROE", linha.get(16).text());
+        rowMap.put("Liq.2meses", linha.get(17).text());
+        rowMap.put("Patrim. Líq", linha.get(18).text());
+        rowMap.put("Dív.Brut/ Patrim.", linha.get(19).text());
+        rowMap.put("Cresc. Rec.5a", linha.get(20).text());
+
+        // Adiciona o mapa à lista
+        data.add(rowMap);
+    }
+
+    // Retorna um papel que foi passado por parâmetro, juntamente com os indicadores
+    public List<Map<String, String>> getPapel(String papel) throws IOException{
+
+        // URL do site da tabela
+        String url = "https://www.fundamentus.com.br/resultado.php";
+
+        // Criar uma conexão e passa parâmetros
+        Connection connection = Jsoup.connect(url).data("negociada", "ON");
+
+        // Conecta ao site via POST e obtém o HTML
+        Document document = connection.post();
+
+        // Selecionar a tabela
+        Element table = document.select("table").first();
+
+        // Cria uma lista para armazenar os dados da tabela
+        List<Map<String, String>> data = new ArrayList<>();
+
+        // Verifica se a tabela foi encontrada
+        if (table != null) {
+
+            // Obtém todas as linhas da tabela
+            Elements linhas_table = table.select("tbody tr");
+
+            // Iterar sobre as linhas da tabela
+            for (Element linha : linhas_table) {
+                // Obtém a célula da linha
+                Elements linha_tr = linha.select("td");
+
+                // Aqui é a condição para verificar se o papel descrito na URL é igual ao da linha da TABLE
+                if (linha_tr.get(0).text().equals(papel.toUpperCase())) {
+
+                    // Chama o método setarDadoDaTabelaNoJson
+                    setarDadoDaTabelaNoJson(linha_tr, data);
+
+                    break;
+
+                }
+            }
+        }
+        // Verifica se a lista está vazia e gera uma exceção
+        if(data.isEmpty()){
+            throw new NoSuchElementException("Parece que você digitou um papel que não existe, verifique se o papel existe e se está escrito corretamente");
+        }
+        // Retornar lista em json
+        return data;
+    }
 
 }
