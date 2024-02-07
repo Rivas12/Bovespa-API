4698848f884c934d7d43b32043e6f6eb3f7257e4
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-07 18:01:27 -0300

    feat: nova class para gerenciar repostas de uma única ação

---
+++ src/main/java/com/bovespaApi/controllers/AcaoController.java
@@ -0,0 +1,30 @@
+package com.bovespaApi.controllers;
+
+
+import com.bovespaApi.services.AcaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.bovespaApi.services.SetorService;
+
+@RestController
+@RequestMapping("/api")
+@Tag(name = "Ação", description = "Retorna informações de uma única ação")
+public class AcaoController {
+
+    // Injeção de dependência do serviço de ações
+    private final AcaoService acaoService;
+
+    @Autowired
+    // Construtor com a injeção de dependência do serviço de ações
+    public AcaoController(AcaoService acaoService) {
+        this.acaoService = acaoService;
+    }
+
+}
---
+++ src/main/java/com/bovespaApi/services/AcaoService.java
@@ -0,0 +1,17 @@
+package com.bovespaApi.services;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.*;
+
+@Service
+public class AcaoService {
+
+
+}
