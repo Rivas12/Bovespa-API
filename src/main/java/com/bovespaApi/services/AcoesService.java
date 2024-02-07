16ba0fe12e29326635f2914cf76d4dad08f85fd6
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-07 12:29:03 -0300

    feat: foi retirado alguns serviços desta class para ser movida na nova

--- src/main/java/com/bovespaApi/services/AcoesService.java
+++ src/main/java/com/bovespaApi/services/AcoesService.java
@@ -54,8 +54,6 @@ public class AcoesService {
 
 
 
-
-
     // Retorna todos os papeis da bolsa
     public List<String> getPapeis() throws IOException {
 
@@ -126,91 +124,6 @@ public class AcoesService {
         return data;
     }
 
-    // Retorna todos os papeis por setor
-    public List<String> getPapeisPorSetorLista(String setor) throws IOException {
-
-        // Verifica se o setor é válido e gerar uma exceção
-        if(Integer.parseInt(setor) > 43 || Integer.parseInt(setor) < 0){
-            throw new IndexOutOfBoundsException("Parece que você digitou um setor que não existe, verifique a documentação para saber os setores disponíveis");
-        }
-
-        // URL do site da tabela
-        String url = "https://www.fundamentus.com.br/resultado.php";
-
-        // Criar uma conexão e passa parâmetros
-        // Cada setor tem uma chave própria; Na documentação tem todos os setores por número
-        Connection connection = Jsoup.connect(url).data("setor", setor).data("negociada", "ON");
-
-        // Conecta ao site via POST e obtém o HTML
-        Document document = connection.post();
-
-        // Seleciona a tabela
-        Element table = document.select("table").first();
-
-        // Cria uma lista para armazenar os dados da tabela
-        List<String> listData = new ArrayList<>();
-
-        // Verifica se a tabela foi encontrada
-        if (table != null) {
-            // Obter todas as linhas da tabela
-            Elements linhas_table = table.select("tbody tr");
-
-            // Itera sobre as linhas da tabela
-            for (Element linha : linhas_table) {
-                // Obtém a célula da linha
-                Elements celula = linha.select("td");
-
-                // Retira o nome da EMPRESA do HTML e adiciona o papel à lista
-                listData.add(celula.get(0).text());
-            }
-        }
-        // Retornar lista
-        return listData;
-    }
-
-    // Retorna todos os papeis junto com os indicadores
-    public List<Map<String, String>> getPapeisPorSetor(String setor) throws IOException {
-
-        // Verifica se o setor é válido e gerar uma exceção
-        if(Integer.parseInt(setor) > 43 || Integer.parseInt(setor) < 0){
-            throw new IndexOutOfBoundsException("Parece que você digitou um setor que não existe, verifique a documentação para saber os setores disponíveis");
-        }
-
-        // URL do site da tabela
-        String url = "https://www.fundamentus.com.br/resultado.php";
-
-        // Criar uma conexão e passa parâmetros
-        // Cada setor tem uma chave própria; na documentação tem todos os setores por número
-        // A chave 'negociada' significa que a ação foi negociada nos últimos 2 meses na bolsa
-        Connection connection = Jsoup.connect(url).data("setor", setor).data("negociada", "ON");
-
-        // Conecta ao site via POST e obtém o HTML
-        Document document = connection.post();
-
-        // Seleciona a tabela
-        Element table = document.select("table").first();
-
-        // Cria uma lista para armazenar os dados da tabela
-        List<Map<String, String>> data = new ArrayList<>();
-
-        // Verifica se a tabela foi encontrada
-        if (table != null) {
-            // Obtém todas as linhas da tabela
-            Elements linhas_table = table.select("tbody tr");
-
-            // Itera sobre as linhas da tabela
-            // O HTML já retorna todos os dados do setor que foram passados, portanto, não é necessário adicionar uma condição
-            for (Element linha : linhas_table) {
-                // Obtenha a célula da linha(tr)
-                Elements linha_tr = linha.select("td");
-
-                // Chama o método setarDadoDaTabelaNoJson
-                setarDadoDaTabelaNoJson(linha_tr, data);
-            }
-        }
-        // Retornar lista em json
-        return data;
-    }
 
     // Retorna um papel que foi passado por parâmetro, juntamente com os indicadores
     public List<Map<String, String>> getPapelComIndicadores(String papel) throws IOException {
