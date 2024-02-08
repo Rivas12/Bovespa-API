5c4f0b9b413b8133b09ac1f8c76ec1a11d3cae4c
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-08 11:41:04 -0300

    feat: nova feature que serve para pesquisar ações com todos os filtros disponiveis

--- src/main/java/com/bovespaApi/controllers/FiltroController.java
+++ src/main/java/com/bovespaApi/controllers/FiltroController.java
@@ -2,13 +2,19 @@ package com.bovespaApi.controllers;
 
 
 import com.bovespaApi.services.FiltroService;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -25,5 +31,133 @@ public class FiltroController {
         this.filtroService = filtroService;
     }
 
+    @GetMapping("/filtro")
+    public List<Map<String, String>> getFiltro(
+            @Parameter(description = "P/L (Preço/Lucro) mínimo")
+            @RequestParam(required = false, defaultValue = "") String pl_min,
 
+            @Parameter(description = "P/L (Preço/Lucro) máximo")
+            @RequestParam(required = false, defaultValue = "") String pl_max,
+
+            @Parameter(description = "P/VP (Preço/Valor Patrimonial) mínimo")
+            @RequestParam(required = false, defaultValue = "") String pvp_min,
+
+            @Parameter(description = "P/VP (Preço/Valor Patrimonial) máximo")
+            @RequestParam(required = false, defaultValue = "") String pvp_max,
+
+            @Parameter(description = "PSR (Preço/Salário da Receita) mínimo")
+            @RequestParam(required = false, defaultValue = "") String psr_min,
+
+            @Parameter(description = "PSR (Preço/Salário da Receita) máximo")
+            @RequestParam(required = false, defaultValue = "") String psr_max,
+
+            @Parameter(description = "Dividend Yield mínimo")
+            @RequestParam(required = false, defaultValue = "") String divy_min,
+
+            @Parameter(description = "Dividend Yield máximo")
+            @RequestParam(required = false, defaultValue = "") String divy_max,
+
+            @Parameter(description = "Patrimônio Líquido Ativos mínimo")
+            @RequestParam(required = false, defaultValue = "") String pativos_min,
+
+            @Parameter(description = "Patrimônio Líquido Ativos máximo")
+            @RequestParam(required = false, defaultValue = "") String pativos_max,
+
+            @Parameter(description = "Preço/Ativo Circulante mínimo")
+            @RequestParam(required = false, defaultValue = "") String pcapgiro_min,
+
+            @Parameter(description = "Preço/Ativo Circulante máximo")
+            @RequestParam(required = false, defaultValue = "") String pcapgiro_max,
+
+            @Parameter(description = "P/EBIT (Preço/EBIT) mínimo")
+            @RequestParam(required = false, defaultValue = "") String pebit_min,
+
+            @Parameter(description = "P/EBIT (Preço/EBIT) máximo")
+            @RequestParam(required = false, defaultValue = "") String pebit_max,
+
+            @Parameter(description = "FGR (Fator de Graham) mínimo")
+            @RequestParam(required = false, defaultValue = "") String fgrah_min,
+
+            @Parameter(description = "FGR (Fator de Graham) máximo")
+            @RequestParam(required = false, defaultValue = "") String fgrah_max,
+
+            @Parameter(description = "Firma EBIT mínima")
+            @RequestParam(required = false, defaultValue = "") String firma_ebit_min,
+
+            @Parameter(description = "Firma EBIT máxima")
+            @RequestParam(required = false, defaultValue = "") String firma_ebit_max,
+
+            @Parameter(description = "Firma EBITDA mínima")
+            @RequestParam(required = false, defaultValue = "") String firma_ebitda_min,
+
+            @Parameter(description = "Firma EBITDA máxima")
+            @RequestParam(required = false, defaultValue = "") String firma_ebitda_max,
+
+            @Parameter(description = "Margem EBIT mínima")
+            @RequestParam(required = false, defaultValue = "") String margemebit_min,
+
+            @Parameter(description = "Margem EBIT máxima")
+            @RequestParam(required = false, defaultValue = "") String margemebit_max,
+
+            @Parameter(description = "Margem Líquida mínima")
+            @RequestParam(required = false, defaultValue = "") String margemliq_min,
+
+            @Parameter(description = "Margem Líquida máxima")
+            @RequestParam(required = false, defaultValue = "") String margemliq_max,
+
+            @Parameter(description = "Liquidez Corrente mínima")
+            @RequestParam(required = false, defaultValue = "") String liqcorr_min,
+
+            @Parameter(description = "Liquidez Corrente máxima")
+            @RequestParam(required = false, defaultValue = "") String liqcorr_max,
+
+            @Parameter(description = "ROIC (Retorno sobre o Capital Investido) mínimo")
+            @RequestParam(required = false, defaultValue = "") String roic_min,
+
+            @Parameter(description = "ROIC (Retorno sobre o Capital Investido) máximo")
+            @RequestParam(required = false, defaultValue = "") String roic_max,
+
+            @Parameter(description = "ROE (Retorno sobre o Patrimônio Líquido) mínimo")
+            @RequestParam(required = false, defaultValue = "") String roe_min,
+
+            @Parameter(description = "ROE (Retorno sobre o Patrimônio Líquido) máximo")
+            @RequestParam(required = false, defaultValue = "") String roe_max,
+
+            @Parameter(description = "Liquidez mínima")
+            @RequestParam(required = false, defaultValue = "") String liq_min,
+
+            @Parameter(description = "Liquidez máxima")
+            @RequestParam(required = false, defaultValue = "") String liq_max,
+
+            @Parameter(description = "Patrimônio Líquido mínimo")
+            @RequestParam(required = false, defaultValue = "") String patrim_min,
+
+            @Parameter(description = "Patrimônio Líquido máximo")
+            @RequestParam(required = false, defaultValue = "") String patrim_max,
+
+            @Parameter(description = "Dívida Bruta mínima")
+            @RequestParam(required = false, defaultValue = "") String divbruta_min,
+
+            @Parameter(description = "Dívida Bruta máxima")
+            @RequestParam(required = false, defaultValue = "") String divbruta_max,
+
+            @Parameter(description = "Taxa de Crescimento de Receita mínima")
+            @RequestParam(required = false, defaultValue = "") String tx_cresc_rec_min,
+
+            @Parameter(description = "Taxa de Crescimento de Receita máxima")
+            @RequestParam(required = false, defaultValue = "") String tx_cresc_rec_max,
+
+            @Parameter(description = "Setor")
+            @RequestParam(required = false, defaultValue = "") String setor,
+
+            @Parameter(description = "Empresa negociada nos últimos 2 meses da bolsa. O parâmetro negociada deve ser ON ou OFF")
+            @RequestParam(required = false, defaultValue = "ON") String negociada,
+
+            @Parameter(description = "Ordem")
+            @RequestParam(required = false, defaultValue = "") String ordem
+
+    ) throws IOException {
+        // Aqui você pode acessar e processar os parâmetros conforme necessário
+        return filtroService.getFiltro(pl_min, pl_max, pvp_min, pvp_max, psr_min, psr_max, divy_min, divy_max, pativos_min, pativos_max, pcapgiro_min, pcapgiro_max, pebit_min, pebit_max, fgrah_min, fgrah_max, firma_ebit_min, firma_ebit_max, firma_ebitda_min, firma_ebitda_max, margemebit_min, margemebit_max, margemliq_min, margemliq_max, liqcorr_min, liqcorr_max, roic_min, roic_max, roe_min, roe_max, liq_min, liq_max, patrim_min, patrim_max, divbruta_min, divbruta_max, tx_cresc_rec_min, tx_cresc_rec_max, setor, negociada, ordem);
+    }
 }
--- src/main/java/com/bovespaApi/services/FiltroService.java
+++ src/main/java/com/bovespaApi/services/FiltroService.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
 import java.util.*;
@@ -48,5 +49,143 @@ public class FiltroService {
         data.add(rowMap);
     }
 
+    public List<Map<String, String>> getFiltro(
+            String plMin,
+            String plMax,
+            String pvpMin,
+            String pvpMax,
+            String psrMin,
+            String psrMax,
+            String divyMin,
+            String divyMax,
+            String pativosMin,
+            String pativosMax,
+            String pcapgiroMin,
+            String pcapgiroMax,
+            String pebitMin,
+            String pebitMax,
+            String fgrahMin,
+            String fgrahMax,
+            String firmaEbitMin,
+            String firmaEbitMax,
+            String firmaEbitdaMin,
+            String firmaEbitdaMax,
+            String margemEbitMin,
+            String margemEbitMax,
+            String margemLiqMin,
+            String margemLiqMax,
+            String liqcorrMin,
+            String liqcorrMax,
+            String roicMin,
+            String roicMax,
+            String roeMin,
+            String roeMax,
+            String liqMin,
+            String liqMax,
+            String patrimMin,
+            String patrimMax,
+            String divbrutaMin,
+            String divbrutaMax,
+            String txCrescRecMin,
+            String txCrescRecMax,
+            String setor,
+            String negociada,
+            String ordem
+    ) throws IOException{
 
+        // URL do site da tabela
+        String url = "https://www.fundamentus.com.br/resultado.php";
+
+        // Lista de variáveis
+        String[] variaveis = {divyMin, divyMax, margemLiqMin, margemLiqMax, firmaEbitdaMin, firmaEbitdaMax, roeMin, roeMax, txCrescRecMin, txCrescRecMax};
+
+        // Tratamento das variáveis
+        for (int i = 0; i < variaveis.length; i++) {
+            if (variaveis[i].isEmpty()) {
+                variaveis[i] = "0.";
+            } else {
+                variaveis[i] = "0." + variaveis[i];
+            }
+        }
+
+        // Criar uma conexão e passa parâmetros
+        Connection connection = Jsoup.connect(url)
+                .data("pl_min", plMin)
+                .data("pl_max", plMax)
+                .data("pvp_min", pvpMin)
+                .data("pvp_max", pvpMax)
+                .data("psr_min", psrMin)
+                .data("psr_max", psrMax)
+                .data("divy_min", divyMin)
+                .data("divy_max", divyMax)
+                .data("pativos_min", pativosMin)
+                .data("pativos_max", pativosMax)
+                .data("pcapgiro_min", pcapgiroMin)
+                .data("pcapgiro_max", pcapgiroMax)
+                .data("pebit_min", pebitMin)
+                .data("pebit_max", pebitMax)
+                .data("fgrah_min", fgrahMin)
+                .data("fgrah_max", fgrahMax)
+                .data("firma_ebit_min", firmaEbitMin)
+                .data("firma_ebit_max", firmaEbitMax)
+                .data("firma_ebitda_min", firmaEbitdaMin)
+                .data("firma_ebitda_max", firmaEbitdaMax)
+                .data("margemebit_min", margemEbitMin)
+                .data("margemebit_max", margemEbitMax)
+                .data("margemliq_min", margemLiqMin)
+                .data("margemliq_max", margemLiqMax)
+                .data("liqcorr_min", liqcorrMin)
+                .data("liqcorr_max", liqcorrMax)
+                .data("roic_min", roicMin)
+                .data("roic_max", roicMax)
+                .data("roe_min", roeMin)
+                .data("roe_max", roeMax)
+                .data("liq_min", liqMin)
+                .data("liq_max", liqMax)
+                .data("patrim_min", patrimMin)
+                .data("patrim_max", patrimMax)
+                .data("divbruta_min", divbrutaMin)
+                .data("divbruta_max", divbrutaMax)
+                .data("tx_cresc_rec_min", txCrescRecMin)
+                .data("tx_cresc_rec_max", txCrescRecMax)
+                .data("setor", setor)
+                .data("negociada", negociada)
+                .data("ordem", ordem);
+
+        // Conecta ao site via POST e obtém o HTML
+        Document document = connection.post();
+
+        // Selecionar a tabela
+        Element table = document.select("table").first();
+
+        // Cria uma lista para armazenar os dados da tabela
+        List<Map<String, String>> data = new ArrayList<>();
+
+        // Verifica se a tabela foi encontrada
+        if (table != null) {
+
+            // Obtém todas as linhas da tabela
+            Elements linhas_table = table.select("tbody tr");
+
+
+                // Obtém todas as linhas da tabela
+                Elements linhas_tabela = table.select("tbody tr");
+
+                // Seta os dados da tabela no Json
+                for (Element linha : linhas_tabela) {
+                    // Obtém a célula da linha
+                    Elements linha_tr = linha.select("td");
+
+                    // Chama o método setarDadoDaTabelaNoJson
+                    setarDadoDaTabelaNoJson(linha_tr, data);
+                }
+
+        }
+        // Verifica se a lista está vazia e gera uma exceção
+        if(data.isEmpty()){
+            throw new NoSuchElementException("Parece que você digitou um papel que não existe, verifique se o papel existe e se está escrito corretamente");
+        }
+        // Retornar lista em json
+        return data;
+    }
 }
