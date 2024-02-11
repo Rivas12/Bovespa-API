7507fcf1b011794358f77dd5035d6faf15da98fc
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-11 17:52:15 -0300

    feat: novo metodo para filtrar acoes ações de forma individual

--- src/main/java/com/bovespaApi/utils/ReduzirCodigo.java
+++ src/main/java/com/bovespaApi/utils/ReduzirCodigo.java
@@ -171,5 +171,96 @@ public class ReduzirCodigo {
         }
     }
 
+    public static void RetornoUnico(String url, String papel, String tipoRetorno, List<Map<String, String>> data) throws IOException {
+        if(tipoRetorno.equals("indicadores")){
+
+            Connection connection = Jsoup.connect(url).data("negociada", "ON");
+
+            Document document = connection.post();
+            Element table = document.select("table").first();
+
+            if (table != null) {
+                Elements linhasTable = table.select("tbody tr");
+
+                for (Element linha : linhasTable) {
+
+                    Elements celula = linha.select("td");
+                    // Aqui é a condição para verificar se o papel descrito na URL é igual ao da linha da TABLE
+                    if (celula.get(0).text().equals(papel.toUpperCase())) {
+
+                        Map<String, String> rowMap = new LinkedHashMap<>();
+
+                        rowMap.put("Papel", celula.get(0).text());
+                        rowMap.put("Empresa", celula.get(0).select("span").attr("title"));
+                        rowMap.put("Cotação", celula.get(1).text());
+                        rowMap.put("P/L", celula.get(2).text());
+                        rowMap.put("PV/P", celula.get(3).text());
+                        rowMap.put("PSR", celula.get(4).text());
+                        rowMap.put("Div.Yield", celula.get(5).text());
+                        rowMap.put("P/Ativo", celula.get(6).text());
+                        rowMap.put("P/Cap.Giro", celula.get(7).text());
+                        rowMap.put("P/EBIT", celula.get(8).text());
+                        rowMap.put("P/Ativo Circ.Liq", celula.get(9).text());
+                        rowMap.put("EV/EBIT", celula.get(10).text());
+                        rowMap.put("EV/EBITDA", celula.get(11).text());
+                        rowMap.put("Mrg Ebit", celula.get(12).text());
+                        rowMap.put("Mrg. Líq.", celula.get(13).text());
+                        rowMap.put("Liq. Corr.", celula.get(14).text());
+                        rowMap.put("ROIC", celula.get(15).text());
+                        rowMap.put("ROE", celula.get(16).text());
+                        rowMap.put("Liq.2meses", celula.get(17).text());
+                        rowMap.put("Patrim. Líq", celula.get(18).text());
+                        rowMap.put("Dív.Brut/ Patrim.", celula.get(19).text());
+                        rowMap.put("Cresc. Rec.5a", celula.get(20).text());
+
+                        data.add(rowMap);
+                    }
+
+                }
+            }
+        } else if (tipoRetorno.equals("proventos")) {
+
+            // Criar uma conexão e passa parâmetros
+            Connection connection = Jsoup.connect(url).data("papel", papel.toUpperCase()).data("negociada", "ON");
+
+            // Conecta ao site via GET e obtém o HTML
+            Document document = connection.get();
+
+            // Selecionar a tabela
+            Element table = document.select("#resultado").first();
+
+            // Verifica se a tabela foi encontrada
+            if (table != null) {
+
+                // Obtém todas as linhas da tabela
+                Elements linhas_table = table.select("tbody tr");
+
+                // Iterar sobre as linhas da tabela
+                for (Element linha : linhas_table) {
+
+                    // Obtém a célula da linha
+                    Elements linha_tr = linha.select("td");
+                    // Cria um mapa para representar a linha
+                    Map<String, String> rowMap = new LinkedHashMap<>();
+
+                    // Adiciona os dados ao mapa
+                    rowMap.put("Valor", linha_tr.get(1).text());
+                    rowMap.put("Tipo", linha_tr.get(2).text());
+                    rowMap.put("Data do anuncio", linha_tr.get(0).text());
+                    rowMap.put("Data de pagamento", linha_tr.get(3).text());
+                    rowMap.put("Por quantas ações", linha_tr.get(4).text());
+
+                    data.add(rowMap);
+                }
+
+            }
+        }
+        // Verifica se a lista está vazia e gera uma exceção
+        if(data.isEmpty()){
+            throw new NoSuchElementException("Parece que você digitou um papel que não existe, verifique se o papel existe e se está escrito corretamente");
+        }
+    }
 
 }
+
+
