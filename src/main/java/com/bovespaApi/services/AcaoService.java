48b65916bea752a0d68558352f78425f2a89c4da
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-11 17:53:27 -0300

    deprac: remoção de metodo pois não está sendo mais usado

--- src/main/java/com/bovespaApi/services/AcaoService.java
+++ src/main/java/com/bovespaApi/services/AcaoService.java
@@ -14,41 +14,6 @@ import java.util.*;
 @Service
 public class AcaoService {
 
-    // SOLID - Single Responsibility Principle
-    // Aqui realiza é setagem de linha(tr) da tabela no Json
-    // A regra de negócio é separada do método principal
-    private void setarDadoDaTabelaNoJson(Elements linha, List<Map<String, String>> data) {
-
-        // Cria um mapa para representar a linha
-        Map<String, String> rowMap = new LinkedHashMap<>();
-
-        // Adiciona os dados ao mapa
-        rowMap.put("Papel", linha.get(0).text());
-        rowMap.put("Empresa", linha.get(0).select("span").attr("title"));
-        rowMap.put("Cotação", linha.get(1).text());
-        rowMap.put("P/L", linha.get(2).text());
-        rowMap.put("PV/P", linha.get(3).text());
-        rowMap.put("PSR", linha.get(4).text());
-        rowMap.put("Div.Yield", linha.get(5).text());
-        rowMap.put("P/Ativo", linha.get(6).text());
-        rowMap.put("P/Cap.Giro", linha.get(7).text());
-        rowMap.put("P/EBIT", linha.get(8).text());
-        rowMap.put("P/Ativo Circ.Liq", linha.get(9).text());
-        rowMap.put("EV/EBIT", linha.get(10).text());
-        rowMap.put("EV/EBITDA", linha.get(11).text());
-        rowMap.put("Mrg Ebit", linha.get(12).text());
-        rowMap.put("Mrg. Líq.", linha.get(13).text());
-        rowMap.put("Liq. Corr.", linha.get(14).text());
-        rowMap.put("ROIC", linha.get(15).text());
-        rowMap.put("ROE", linha.get(16).text());
-        rowMap.put("Liq.2meses", linha.get(17).text());
-        rowMap.put("Patrim. Líq", linha.get(18).text());
-        rowMap.put("Dív.Brut/ Patrim.", linha.get(19).text());
-        rowMap.put("Cresc. Rec.5a", linha.get(20).text());
-
-        // Adiciona o mapa à lista
-        data.add(rowMap);
-    }
 
     // Retorna um papel que foi passado por parâmetro, juntamente com os indicadores
     public List<Map<String, String>> getPapel(String papel) throws IOException{
