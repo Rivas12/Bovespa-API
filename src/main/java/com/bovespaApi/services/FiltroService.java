5069129e49843fbb2cfa08186ee5497160f70280
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-11 15:43:29 -0300

    feat: codigo limpo usando a Class auxiliar

--- src/main/java/com/bovespaApi/services/FiltroService.java
+++ src/main/java/com/bovespaApi/services/FiltroService.java
@@ -1,5 +1,7 @@
 package com.bovespaApi.services;
 
+import com.bovespaApi.utils.ReduzirCodigo;
+
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -7,166 +9,27 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
-import java.net.SocketOption;
 import java.util.*;
 
 @Service
 public class FiltroService {
 
-    public List<Map<String, String>> getFiltro(
-            String plMin,
-            String plMax,
-            String pvpMin,
-            String pvpMax,
-            String psrMin,
-            String psrMax,
-            String divyMin,
-            String divyMax,
-            String pativosMin,
-            String pativosMax,
-            String pcapgiroMin,
-            String pcapgiroMax,
-            String pebitMin,
-            String pebitMax,
-            String fgrahMin,
-            String fgrahMax,
-            String firmaEbitMin,
-            String firmaEbitMax,
-            String firmaEbitdaMin,
-            String firmaEbitdaMax,
-            String margemEbitMin,
-            String margemEbitMax,
-            String margemLiqMin,
-            String margemLiqMax,
-            String liqcorrMin,
-            String liqcorrMax,
-            String roicMin,
-            String roicMax,
-            String roeMin,
-            String roeMax,
-            String liqMin,
-            String liqMax,
-            String patrimMin,
-            String patrimMax,
-            String divbrutaMin,
-            String divbrutaMax,
-            String txCrescRecMin,
-            String txCrescRecMax,
-            String setor,
-            String negociada,
-            String ordem
-    ) throws IOException{
+    public List<Map<String, String>> getFiltro(String plMin, String plMax, String pvpMin, String pvpMax, String psrMin, String psrMax, String divyMin, String divyMax, String pativosMin, String pativosMax, String pcapgiroMin, String pcapgiroMax, String pebitMin, String pebitMax, String fgrahMin, String fgrahMax, String firmaEbitMin, String firmaEbitMax, String firmaEbitdaMin, String firmaEbitdaMax, String margemEbitMin, String margemEbitMax, String margemLiqMin, String margemLiqMax, String liqcorrMin, String liqcorrMax, String roicMin, String roicMax, String roeMin, String roeMax, String liqMin, String liqMax, String patrimMin, String patrimMax, String divbrutaMin, String divbrutaMax, String txCrescRecMin, String txCrescRecMax, String setor, String negociada, String ordem) throws IOException {
 
-        // URL do site da tabela
-        String url = "https://www.fundamentus.com.br/resultado.php";
+        // Cria um mapa para armazenar os dados do POST
+        Map<String, String> dadosPost = new HashMap<>();
+        String[] chaves = {"pl_min", "pl_max", "pvp_min", "pvp_max", "psr_min", "psr_max", "divy_min", "divy_max", "pativos_min", "pativos_max", "pcapgiro_min", "pcapgiro_max", "pebit_min", "pebit_max", "fgrah_min", "fgrah_max", "firma_ebit_min", "firma_ebit_max", "firma_ebitda_min", "firma_ebitda_max", "margemebit_min", "margemebit_max", "margemliq_min", "margemliq_max", "liqcorr_min", "liqcorr_max", "roic_min", "roic_max", "roe_min", "roe_max", "liq_min", "liq_max", "patrim_min", "patrim_max", "divbruta_min", "divbruta_max", "tx_cresc_rec_min", "tx_cresc_rec_max", "setor", "negociada", "ordem"};
+        String[] valores = {plMin, plMax, pvpMin, pvpMax, psrMin, psrMax, divyMin, divyMax, pativosMin, pativosMax, pcapgiroMin, pcapgiroMax, pebitMin, pebitMax, fgrahMin, fgrahMax, firmaEbitMin, firmaEbitMax, firmaEbitdaMin, firmaEbitdaMax, margemEbitMin, margemEbitMax, margemLiqMin, margemLiqMax, liqcorrMin, liqcorrMax, roicMin, roicMax, roeMin, roeMax, liqMin, liqMax, patrimMin, patrimMax, divbrutaMin, divbrutaMax, txCrescRecMin, txCrescRecMax, setor, negociada, ordem};
 
-
-        // Criar uma conexão e passa parâmetros
-        Connection connection = Jsoup.connect(url)
-                .data("pl_min", plMin)
-                .data("pl_max", plMax)
-                .data("pvp_min", pvpMin)
-                .data("pvp_max", pvpMax)
-                .data("psr_min", psrMin)
-                .data("psr_max", psrMax)
-                .data("divy_min", divyMin)
-                .data("divy_max", divyMax)
-                .data("pativos_min", pativosMin)
-                .data("pativos_max", pativosMax)
-                .data("pcapgiro_min", pcapgiroMin)
-                .data("pcapgiro_max", pcapgiroMax)
-                .data("pebit_min", pebitMin)
-                .data("pebit_max", pebitMax)
-                .data("fgrah_min", fgrahMin)
-                .data("fgrah_max", fgrahMax)
-                .data("firma_ebit_min", firmaEbitMin)
-                .data("firma_ebit_max", firmaEbitMax)
-                .data("firma_ebitda_min", firmaEbitdaMin)
-                .data("firma_ebitda_max", firmaEbitdaMax)
-                .data("margemebit_min", margemEbitMin)
-                .data("margemebit_max", margemEbitMax)
-                .data("margemliq_min", margemLiqMin)
-                .data("margemliq_max", margemLiqMax)
-                .data("liqcorr_min", liqcorrMin)
-                .data("liqcorr_max", liqcorrMax)
-                .data("roic_min", roicMin)
-                .data("roic_max", roicMax)
-                .data("roe_min", roeMin)
-                .data("roe_max", roeMax)
-                .data("liq_min", liqMin)
-                .data("liq_max", liqMax)
-                .data("patrim_min", patrimMin)
-                .data("patrim_max", patrimMax)
-                .data("divbruta_min", divbrutaMin)
-                .data("divbruta_max", divbrutaMax)
-                .data("tx_cresc_rec_min", txCrescRecMin)
-                .data("tx_cresc_rec_max", txCrescRecMax)
-                .data("setor", setor)
-                .data("negociada", negociada)
-                .data("ordem", ordem);
-
-
-
-        // Conecta ao site via POST e obtém o HTML
-        Document document = connection.post();
-
-        // Selecionar a tabela
-        Element table = document.select("table").first();
-
-        // Cria uma lista para armazenar os dados da tabela
-        List<Map<String, String>> data = new ArrayList<>();
-
-        // Verifica se a tabela foi encontrada
-        if (table != null) {
-
-                // Obtém todas as linhas da tabela
-                Elements linhas_tabela = table.select("tbody tr");
-
-                // Seta os dados da tabela no Json
-                for (Element linha : linhas_tabela) {
-                    // Obtém a célula da linha
-                    Elements linha_tr = linha.select("td");
-
-                    // Cria um mapa para representar a linha
-                    Map<String, String> rowMap = new LinkedHashMap<>();
-
-                    // Adiciona os dados ao mapa
-                    rowMap.put("Papel", linha_tr.get(0).text());
-                    rowMap.put("Empresa", linha_tr.get(0).select("span").attr("title"));
-                    rowMap.put("Cotação", linha_tr.get(1).text());
-                    rowMap.put("P/L", linha_tr.get(2).text());
-                    rowMap.put("PV/P", linha_tr.get(3).text());
-                    rowMap.put("PSR", linha_tr.get(4).text());
-                    rowMap.put("Div.Yield", linha_tr.get(5).text());
-                    rowMap.put("P/Ativo", linha_tr.get(6).text());
-                    rowMap.put("P/Cap.Giro", linha_tr.get(7).text());
-                    rowMap.put("P/EBIT", linha_tr.get(8).text());
-                    rowMap.put("P/Ativo Circ.Liq", linha_tr.get(9).text());
-                    rowMap.put("EV/EBIT", linha_tr.get(10).text());
-                    rowMap.put("EV/EBITDA", linha_tr.get(11).text());
-                    rowMap.put("Mrg Ebit", linha_tr.get(12).text());
-                    rowMap.put("Mrg. Líq.", linha_tr.get(13).text());
-                    rowMap.put("Liq. Corr.", linha_tr.get(14).text());
-                    rowMap.put("ROIC", linha_tr.get(15).text());
-                    rowMap.put("ROE", linha_tr.get(16).text());
-                    rowMap.put("Liq.2meses", linha_tr.get(17).text());
-                    rowMap.put("Patrim. Líq", linha_tr.get(18).text());
-                    rowMap.put("Dív.Brut/ Patrim.", linha_tr.get(19).text());
-                    rowMap.put("Cresc. Rec.5a", linha_tr.get(20).text());
-
-                    // Adiciona o mapa à lista
-                    data.add(rowMap);
-                }
-
-        } else {
-            // Se a tabela não foi encontrada, lança uma exceção
-            throw new NoSuchElementException("Não foi possível encontrar a tabela, verifique se o site está funcionando corretamente");
+        // Adiciona os pares de chave-valor ao mapa
+        for (int i = 0; i < chaves.length; i++) {
+            dadosPost.put(chaves[i], valores[i]);
         }
 
-        // Retornar lista em json
+        List<Map<String, String>> data = new ArrayList<>();
+        ReduzirCodigo.EncontrarTabelaEAdicionarALista("https://www.fundamentus.com.br/resultado.php", "indicadores", data, dadosPost);
         return data;
     }
 
