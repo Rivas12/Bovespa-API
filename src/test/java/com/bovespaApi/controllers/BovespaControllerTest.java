29cefdd7e774a693c716d29966b58dd4fcae4c7c
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-16 17:47:04 -0300

    feat: cria teste o controlador do endpoint /api/bovespa/papeis

---
+++ src/test/java/com/bovespaApi/controllers/BovespaControllerTest.java
@@ -0,0 +1,29 @@
+package com.bovespaApi.controllers;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BovespaControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Testa o endpoint /bovespa/papeis
+    @Test
+    public void testGetPapeisEndpoint() throws Exception {
+        mockMvc.perform(get("/api/bovespa/papeis"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray());
+    }
+}
