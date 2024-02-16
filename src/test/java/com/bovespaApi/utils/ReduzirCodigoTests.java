bc40450269688f3c0e76b226fead21645db82800
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-15 22:55:37 -0300

    feat: cria teste para o metodo EncontrarTabelaEAdicionarALista

--- src/test/java/com/bovespaApi/utils/ReduzirCodigoTests.java
+++ src/test/java/com/bovespaApi/utils/ReduzirCodigoTests.java
@@ -1,6 +1,23 @@
 package com.bovespaApi.utils;
 
-class ReduzirCodigoTests {
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReduzirCodigoTests{
+
+    @Test
+    void testEncontrarTabelaEAdicionarALista() throws IOException  {
+        Map<String, String> dadosPost = new HashMap<>();
+        List<String> data = new ArrayList<>();
+        ReduzirCodigo.EncontrarTabelaEAdicionarALista("https://www.fundamentus.com.br/resultado.php", "papeis", data, dadosPost);
+        assertFalse(data.isEmpty());
+    }
 
 }
