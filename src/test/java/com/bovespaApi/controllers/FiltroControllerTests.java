751975bdcf39fb559b658bab6d1a90957b5b2355
Rivas12 <rivaldosilveira13@gmail.com>
2024-02-16 19:51:23 -0300

    feat: cria teste o controlador do endpoint /api/filtro

---
+++ src/test/java/com/bovespaApi/controllers/FiltroControllerTests.java
@@ -0,0 +1,30 @@
+package com.bovespaApi.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FiltroControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetFiltrosEndpoint() throws Exception {
+        mockMvc.perform(get("/api/filtro")
+                        .param("pvp_max", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+
+
+}
